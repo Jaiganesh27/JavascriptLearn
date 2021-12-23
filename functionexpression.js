66b994e8541a5a68683470e14f96b3a56498aead
jaiganesh27 <jaiganeshveeramani@gmail.com>
2021-12-23 18:53:23 +0530

    changes

---
+++ functionexpression.js
@@ -0,0 +1,15 @@
+// while hoisting a function declaration is accepted when we call it on before declaration or after declaration
+// but when we call a function expression hoisting only accepted when we call after it declared or initialised
+// otherwise it will make an error.
+
+func1();
+function func1(){
+    console.log("function one called!!");
+}
+func2(5,6);
+var func2 = function (a,b){
+    let c = a + b;
+    console.log("c",c);
+}
+
+
