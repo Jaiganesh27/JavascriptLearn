1792118c59e4044880d81d2f3824ca2e21e96e06
jaiganesh27 <jaiganeshveeramani@gmail.com>
2021-12-23 19:32:56 +0530

    changes

---
+++ hoistingvariable.js
@@ -0,0 +1,12 @@
+
+testing();
+var a = 10;
+let b;
+const c = 30;
+let names = "gopi";
+function testing(){
+    let name = "gopi123";
+    console.log(name);
+}
+console.log(names);
+console.log(c);
