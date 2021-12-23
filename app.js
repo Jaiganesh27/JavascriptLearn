c7e99c992c5f2101b83123b820fb4cc98447b185
jaiganesh27 <jaiganeshveeramani@gmail.com>
2021-12-23 18:42:48 +0530

    changes

--- app.js
+++ app.js
@@ -1,5 +1,24 @@
+// Execution context 
+// Execution Context are two types 
+// Global execution context and functional execution context
+// Javascript program always run with global execution firstly
+// Then if any functions has been called it will create Execution context stack 
+// execution context will created and added to the top of the ecs and then it has been changed
+// will  complete the execution context it will popped from ecs
+// based on this execution context works!! 
+function func1(){
 console.log("hii");
-var a=10;
-var b=20;
-var c = a+b;
-console.log("add of a + b is :",c);
+}
+function func2(){
+    func3();
+}
+function func3(){
+func4();
+}
+function func4(){
+func1();
+}
+function func5(){
+func2();
+}
+func5();
