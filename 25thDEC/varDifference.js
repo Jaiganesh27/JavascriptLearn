b676d3bf949b0bc3a64306dc5e299b030468cfd5
jaiganesh27 <jaiganeshveeramani@gmail.com>
2021-12-26 13:20:23 +0530

    changes

---
+++ 25thDEC/varDifference.js
@@ -0,0 +1,37 @@
+//without initilising
+var testVar;
+let testLet;
+const testConst = 5455;
+console.log(
+  'testvar',
+  typeof testVar,
+  'testLet',
+  typeof testLet,
+  'testCosnt',
+  typeof testConst
+);
+
+// with initialising
+// var testVar = 'it is var string';
+// let testLet = 'it is let string';
+// const testConst  = 'it is const sting';
+
+//redeclare in samescpe
+// var testVar = 41;
+// let testLet = 31;
+// const testConst = 3443;
+
+//redeclare in localscope
+// function testinVariables(){
+//    var testVar = 41;
+//    let testLet = 31;
+//    const testConst = 3443;
+//    console.log("testVarConsole",testVar);
+//    console.log("testLetConsole",testLet);
+//    console.log("testConstConsole",testConst);
+// }
+
+//reassigning or chage values of variable
+// testVar = 41;
+// testLet = 31;
+// testConst = 3443;
