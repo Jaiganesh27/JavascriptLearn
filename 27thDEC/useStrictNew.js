fe09a661d81efb305de2b9a55788333708002624
jaiganesh27 <jaiganeshveeramani@gmail.com>
2021-12-29 11:15:13 +0530

    changes

--- 27thDEC/useStrictNew.js
+++ 27thDEC/useStrictNew.js
@@ -5,7 +5,7 @@ let d = new String('my name is jai');
 console.log("d",d);
 let curdate = new Date();
 console.log("current date",curdate);
-const = animals = {
+const animals = {
   pet:'cat',
   wild:'lion',
   bird:'crow',
