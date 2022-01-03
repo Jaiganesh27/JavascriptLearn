075a247d6316302666874aa94c0fabbf28163127
jaiganesh27 <jaiganeshveeramani@gmail.com>
2022-01-03 08:29:49 +0530

    changes

--- 31stDEC/forin.js
+++ 31stDEC/forin.js
@@ -1,11 +1,27 @@
-const object = { a: 1, b: 2, c: 3 };
-const arrays = [1,2,3,4,5,6];
-for (const property in object) {
-  console.log(property);
-}
-for (const property of object) {
+const object = { name: 'key', value: 'value', pair: 'pair' };
+const arrays = ['jai',21,'chennai'];
+const str = 'StringValue';
+// for (const property of object) {
+//   console.log(property);
+// }
+for (const property of arrays) {
     console.log(property);
   }
 
+for(let value of str){
+  console.log(value);
+}
+// generator function
+function* generatorFunc() {
+  
+  yield 10;
+  yield 'google';
+  yield 30;
+}
 
-// for
+const obj = generatorFunc();
+
+// iteration through generator
+for (let value of obj) {
+  console.log(value);
+}
