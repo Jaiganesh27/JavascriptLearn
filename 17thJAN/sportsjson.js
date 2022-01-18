d7e1262f4c9d2ac333f2bd6ef2e60e44a3f09e31
jaiganesh27 <jaiganeshveeramani@gmail.com>
2022-01-18 11:04:50 +0530

    sportsjson

---
+++ 17thJAN/sportsjson.js
@@ -0,0 +1,57 @@
+cricket = {
+    teams:[
+        {
+           teamName:"Australia",
+           ownGround:"sydney",
+           coach:{
+               batCoach:"rickyponting",
+               bowlCoach:"symonds",
+               headCoach:"mike hussey",
+            },
+           playing11:[
+               {
+                 captain:true,
+                 playerName : "SteveSmith",
+                 age:"30",
+                 playerType:"Batsman",
+                 acheivements:[{
+                     achievementName:"worldcup50",
+                     year:"2015",
+                 }]
+
+               }
+        ],
+            trophies:[
+                {trophyName:"worldcup50",year:"2015",opponent:"NewZealand"}
+            ]
+
+        },
+        {
+            teamName:"India",
+            ownGround:"Chennai",
+            coach:{
+                batCoach:"SachinTendulkar",
+                bowlCoach:"Nehra",
+                headCoach:"MSDhoni",
+             },
+            playing11:[
+                {
+                  captain:true,
+                  playerName : "Virat Kohli",
+                  age:"31",
+                  playerType:"Batsman",
+                  acheivements:[{
+                      achievementName:"worldcup50",
+                      year:"2011",
+                  }]
+ 
+                }
+         ],
+             trophies:[
+                 {trophyName:"worldcup50",year:"2011",opponent:"SriLanka"}
+             ]
+ 
+         }
+
+    ]
+}
