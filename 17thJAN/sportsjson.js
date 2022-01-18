4f7f9484a027acad34b14ab858e5ca3bd9838fd3
jaiganesh27 <jaiganeshveeramani@gmail.com>
2022-01-18 15:09:35 +0530

    sportsjson

--- 17thJAN/sportsjson.js
+++ 17thJAN/sportsjson.js
@@ -3,6 +3,7 @@ cricket = {
         {
            teamName:"Australia",
            ownGround:"sydney",
+           ranking:2,
            coach:{
                batCoach:"rickyponting",
                bowlCoach:"symonds",
@@ -19,16 +20,31 @@ cricket = {
                      year:"2015",
                  }]
 
-               }
+               },
+               {
+                captain:false,
+                playerName : "Mitchell Starc",
+                age:"31",
+                playerType:"Bowler",
+                acheivements:[{
+                    achievementName:"worldcup50",
+                    year:"2015",
+                }]
+
+              }
         ],
             trophies:[
-                {trophyName:"worldcup50",year:"2015",opponent:"NewZealand"}
+                {trophyName:"worldcup50",year:"2015",opponent:"NewZealand"},
+                {trophyName:"worldcup20",year:"2021",opponent:"NewZealand"},
+                {trophyName:"worldcup50",year:"2007",opponent:"India"},
+                {trophyName:"worldcup50",year:"2003",opponent:"Pakistan"}
             ]
 
         },
         {
             teamName:"India",
             ownGround:"Chennai",
+            ranking:1,
             coach:{
                 batCoach:"SachinTendulkar",
                 bowlCoach:"Nehra",
@@ -45,10 +61,62 @@ cricket = {
                       year:"2011",
                   }]
  
-                }
+                },
+                {
+                    captain:false,
+                    playerName : "Ravindra Jadeja",
+                    age:"30",
+                    playerType:"AllRounder",
+                    acheivements:[{
+                        achievementName:"worldcup20",
+                        year:"2007",
+                    }]
+   
+                  }
          ],
              trophies:[
-                 {trophyName:"worldcup50",year:"2011",opponent:"SriLanka"}
+                 {trophyName:"worldcup50",year:"2011",opponent:"SriLanka"},
+                 {trophyName:"worldcup20",year:"2007",opponent:"Pakistan"},
+                 {trophyName:"worldcupCtrophy",year:"2013",opponent:"England"}
+             ]
+ 
+         },
+         {
+            teamName:"England",
+            ownGround:"London",
+            ranking:3,
+            coach:{
+                batCoach:"Strauss",
+                bowlCoach:"Broad",
+                headCoach:"Vaghen",
+             },
+            playing11:[
+                {
+                  captain:true,
+                  playerName : "Joe Root",
+                  age:"29",
+                  playerType:"Batsman",
+                  acheivements:[{
+                      achievementName:"worldcup50",
+                      year:"2019",
+                  }]
+ 
+                },
+                {
+                    captain:false,
+                    playerName : "Sam Curran",
+                    age:"25",
+                    playerType:"AllRounder",
+                    acheivements:[{
+                        achievementName:"worldcup50",
+                        year:"2019",
+                    }]
+   
+                  }
+         ],
+             trophies:[
+                 {trophyName:"worldcup50",year:"2019",opponent:"NewZealand"},
+                 {trophyName:"worldcup20",year:"2009",opponent:"Pakistan"}
              ]
  
          }
@@ -57,6 +125,10 @@ cricket = {
 }
 console.log(cricket);
 
+
+
+
+
 // ADD,UPDATE,DELETE
 // teams add update delete
 // playing 11 add , update ,delete
@@ -64,10 +136,146 @@ console.log(cricket);
 // player individual achievement add update delete
 
 // GET 
+// show playing 11 of a teams      - done
+// show captains of all teams         - done
+// show youngest captain and older captain  - done
+// show most tryphy team  - done
+// show team name , own ground and head coach of all teams. - done
+// show recent trophy of the one teams. 
+// sort team tropies count
+
+
+
 // show playing 11 of a teams
+cricket.teams.forEach((element) =>{
+    console.log("Team Name  :", element.teamName);
+})
+
+
+
 // show captains of all teams
+let tempArr=[];
+tempArr = cricket.teams.map((element) =>{
+         return element.playing11.filter((ele) =>
+         {
+           if(ele.captain == true)
+           return  ele;
+         });
+})
+console.log(tempArr);
+
 // show youngest captain and older captain
-// show most tryphy team
-// show most achievable player in a one team
-// show team name , own ground and head coach of all teams.
-// show recent trophy of the one teams.
+let captainArr = [];
+ cricket.teams.forEach((element) =>{
+    return element.playing11.forEach((ele) =>
+    {
+      if(ele.captain == true)
+      captainArr.push({"teamName":element.teamName,"CaptainName":ele.playerName,"CaptainAge":ele.age});
+    });
+})
+console.log(captainArr);
+    sortedCaptains = captainArr.sort((vala,valb)=>{
+         return valb.CaptainAge - vala.CaptainAge 
+    })
+console.log(`Young Captain is ${sortedCaptains[sortedCaptains.length-1].CaptainName}`);
+console.log(`Elder Captain is ${sortedCaptains[0].CaptainName}`);
+
+// show teamname , own ground and head coach of all teams
+
+let teamData =[];
+teamData = cricket.teams.map((elem)=>{
+    return  {"teamName":elem.teamName,"OwnGround":elem.ownGround,"HeadCoach":elem.coach.headCoach};
+})
+console.table(teamData);
+
+
+// checking all teams have their ranking
+boolRanking = cricket.teams.every((element)=>{
+      return element.ranking != null;
+})
+console.log(boolRanking);
+
+//most trophies of each teams
+temp = cricket.teams.map((element)=>{
+   return {"TeamName":element.teamName,"Trophies":element.trophies.length}
+}) 
+// trophy wise ascending order
+console.log(temp.sort((a,b) => {
+return b.Trophies - a.Trophies 
+}));
+
+
+// show playing 11 of particular team   
+let play11;
+function  getPlaying11(tName){
+cricket.teams.forEach((element,index)=>{
+       if(element.teamName == tName){
+          play11 = element.playing11.map((ele)=>{
+              return {playerName:ele.playerName,playerType:ele.playerType}
+          })
+        }
+    })
+}
+getPlaying11('India');
+console.log(play11);
+
+let recentArr =[];
+// recent trophy of team
+cricket.teams.forEach((ele)=>{
+    let temp;
+    temp = ele.trophies.sort((fil1,fil2)=>{
+        return fil1.year - fil2.year
+    })
+    temp.TeamName = ele.teamName;
+    console.log(temp);
+    recentArr.push(temp);
+    
+
+})
+recentTrophy = recentArr.map((elem)=>{ 
+    return elem[elem.length-1]
+})
+console.log(recentArr);
+console.log("Recent Trophy",recentTrophy);
+
+
+function addPlaying11(cap,playname,playage,playtype,playachieve){
+    Playing11Obj = {
+        captain:null,
+        playerName : null,
+        age:null,
+        playerType:null,
+        achievements:[]
+      }
+      Playing11Obj.captain = cap;
+      Playing11Obj.playerName = playname;
+      Playing11Obj.age = playage;
+      Playing11Obj.playerType = playtype;
+      for(let val in playachieve)
+      Playing11Obj.achievements.push(playachieve[val]);
+      return Playing11Obj;
+}
+addPlayerIntoTeam('India',false,'Rohit Sharma',33,'Opener',[playerIndiAchieve('worldcup',2007),playerIndiAchieve('ThriceDoubleCentury',2014)])
+function addPlayerIntoTeam(team,cap,playname,playage,playtype,playachieve){
+   cricket.teams.forEach((ele)=>{
+       if(ele.teamName === team){
+       temp = addPlaying11(cap,playname,playage,playtype,playachieve);  
+       ele.playing11.push(temp);
+    }
+   })
+}
+function addTrophy(){
+
+}
+function addTeam(){
+
+}
+function playerIndiAchieve(achiveName,AchivYear){
+AchieveObj = {
+    achievementName:null,
+    year:null
+}
+AchieveObj.achievementName = achiveName;
+AchieveObj.year = AchivYear;
+return AchieveObj;
+}
