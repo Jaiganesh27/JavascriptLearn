bc105daca59d50725a9147c4594216550caf2300
jaiganesh27 <jaiganeshveeramani@gmail.com>
2022-02-03 19:19:22 +0530

    changes

--- sequelize/controllers/productController.js
+++ sequelize/controllers/productController.js
@@ -1,21 +1,166 @@
+const { Op } = require('sequelize');
 const db = require('../models/index.js');
 const Product = db.products;
 
-exports.create = async (req,res)=>{
-    console.log("req in create ",req);
+addProduct = async (req, res) => {
+    console.log("req in create ", req);
     const info = {
-     title: req.body.title,
-     price:req.body.price,
-     description:req.body.description,
-     published:req.body.published    
+        title: req.body.title,
+        price: req.body.price,
+        description: req.body.description,
+        published: req.body.published
     };
     console.log(info)
-    try{
-     addProd = await Product.create(info);
-     data = await addProd.json();
-     res.status(200).send(addProd)
-    }
-    catch(err){
-     res.status(500).send(err);
-    }
+
+    console.log("try block");
+    let addProd = await Product.create(info)
+    console.log("data", addProd);
+    res.status(200).send(addProd)
+}
+
+getAllProduct = async (req, res) => {
+    let getAll = await Product.findAll({
+
+    });
+    res.status(200).send(getAll);
+}
+getOneProduct = async (req, res) => {
+    let id = req.params.id;
+    let getAll = await Product.findOne({
+        where: {
+            id: id
+        }
+    });
+    res.status(200).send(getAll);
+}
+deleteProduct = async (req, res) => {
+    let id = req.params.id;
+    console.log("req.params.id", id)
+    let getAll = await Product.destroy({
+        where: {
+            id: id
+        }
+    });
+    res.status(200).send(getAll);
+}
+updateProduct = async (req, res) => {
+    let id = req.params.id;
+    let getAll = await Product.update(req.body, {
+        where: {
+            id: id
+        }
+    });
+    res.status(200).send(getAll);
+}
+getSpecificAttrib = async (req, res) => {
+    let getAll = await Product.findAll({
+        //   attributes:['title','price'] normal attrib
+        //   attributes:['title',['description','desc'],'price'] alias name
+        //   attributes:[[sequelize.fn('COUNT',sequelize.col('price')),'price_count']]
+    });
+    res.status(200).send(getAll);
+}
+getWhereClause = async (req, res) => {
+    let id = req.params.id;
+    let getAll = await Product.findAll({
+        where: {
+            // id:id //basic default for = operator
+            id: {
+                // [Op.ne]:id
+                // [Op.eq]:id
+                // [Op.]
+            }
+        }
+    });
+    res.status(200).send(getAll);
+}
+
+Finders = async (req, res) => {
+    let id = req.params.id;
+    let title = req.body.title;
+    // let  getAll = await Product.findByPk(id);
+
+
+    // let  getAll = await Product.findAll({
+    //     where:{
+    //         id:id
+    //     }
+    // });
+
+
+    let getAll = await Product.findAll({
+        where: {
+            // eq,or,and,ne,
+            // [Op.or]:[{published:true},{id:id}]
+            // [Op.and]:[{published:true},{id:id}]
+
+            // published:{
+                // [Op.ne]:false
+            // }
+
+            // id:{
+            // [Op.lte]:id
+            // [Op.ne]:id
+            // [Op.lt]:id
+            // [Op.gte]:id
+            // [Op.gt]:id
+            // [Op.between]:[10,15]
+            // [Op.notBetween]:[10,15]
+            // },
+
+            title:{
+            // [Op.in]: ["RealMe", "OnePlus"]
+            // [Op.notIn]: ["RealMe", "OnePlus"]
+            // [Op.like]: '%One'  
+            // [Op.like]: 'One%'     
+            // [Op.like]: '%One%'              
+            // [Op.notLike]: '%One%'                 
+            // [Op.startsWith]: 'One'                 
+            // [Op.endsWith]: 'One'                  
+            // [Op.substring]: 'One'
+            // [Op.regexp]: '^[h|a|b]'
+            // [Op.notRegexp]: '^[h|a|t]', 
+            
+            }
+    
+        }
+    });
+
+    // let  [getAll,BoolVal] = await Product.findOrCreate({
+    //     where:{
+    //             title:title,
+    //         },
+    //     defaults:{
+    //         price:500,
+    //         published:false,
+    //     }
+    // });
+    // console.log('getall',getAll)
+    //   console.log("Boolvalue",BoolVal);
+      res.status(200).send(getAll);
+
+    //   const { count , rows } = await Product.findAndCountAll({
+    //       where:{
+    //          id:id
+    //       }, 
+    //     offset:0,
+    //     limit:10
+    // });
+    // console.log('count',count);
+    // console.log(rows)
+    // res.status(200).send(rows);
+
+
+}
+
+
+module.exports = {
+    addProduct,
+    updateProduct,
+    deleteProduct,
+    getAllProduct,
+    getOneProduct,
+    getSpecificAttrib,
+    getWhereClause,
+    Finders
 }
--- sequelize/routes/productRouter.js
+++ sequelize/routes/productRouter.js
@@ -1,7 +1,12 @@
 const express = require('express')
 const router = express.Router()
 const productControl = require('../controllers/productController.js')
-router.post('/addProduct',productControl.create)
-
+router.post('/addProduct',productControl.addProduct);
+router.put('/updateProduct/:id',productControl.updateProduct);
+router.delete('/deleteProduct/:id',productControl.deleteProduct);
+router.get('/getOneProduct/:id',productControl.getOneProduct);
+router.get('/getSpecificAttrib',productControl.getSpecificAttrib);
+router.get('/getWhereClause/:id',productControl.getWhereClause);
+router.get('/Finders/:id',productControl.Finders);
 
 module.exports = router;
